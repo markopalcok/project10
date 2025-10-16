6096e155ff9b234a9b89b2671f6d6f1694e07d68
Marko Palcok <marko.palcok@ibmix.hr>
2025-10-16 15:30:59 +0200

    Add interface test

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
 public class Main {
     public static void main(String[] args){
-        QuestionService service = new QuestionService();
-        service.playQuiz();
-        service.printScore();
+//        QuestionService service = new QuestionService();
+//        service.playQuiz();
+//        service.printScore();
+
+        Rabbit rabbit = new Rabbit();
+        Hawk hawk = new Hawk();
+        Fish fish = new Fish();
+
+        rabbit.flee();
+        hawk.hunt();
+
+        fish.flee();
+        fish.hunt();
+
     }
 }
