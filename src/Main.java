c3845dd3c4c27e4811c1512e9adb23a22901b563
Marko Palcok <marko.palcok@ibmix.hr>
2025-10-16 13:35:36 +0200

    create questions game

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args){
         QuestionService service = new QuestionService();
-        service.displayQuestions();
+        service.playQuiz();
+        service.printScore();
     }
 }
--- src/QuestionService.java
+++ src/QuestionService.java
@@ -1,18 +1,52 @@
+import java.util.Scanner;
+
 public class QuestionService{
     Question[] questions = new Question[5];
+    Scanner input = new Scanner(System.in);
+    String[] selection = new String[5];
 
     QuestionService(){
-        questions[0] = new Question(1, "what", "a", "b","c","d", "answoer");
-        questions[1] = new Question(2, "what2", "a", "b","c","d", "answoer2");
-        questions[2] = new Question(3, "what3", "a", "b","c","d", "answoer3");
-        questions[3] = new Question(4, "what4", "a", "b","c","d", "answoer4");
-        questions[4] = new Question(5, "what5", "a", "b","c","d", "answoer5");
+        questions[0] = new Question(1, "Size of int?", "2", "6","4","8", "4");
+        questions[1] = new Question(2, "Size of double?", "2", "6","4","8", "8");
+        questions[2] = new Question(3, "size of char?", "2", "6","4","8", "4");
+        questions[3] = new Question(4, "Size of long?", "2", "6","4","8", "6");
+        questions[4] = new Question(5, "Size of boolean?", "1", "6","4","8", "1");
     }
 
-    public void displayQuestions(){
+    public void playQuiz() {
+        int i = 0;
         for(Question q : questions){
-            System.out.println(q.toString());
+            System.out.println("Question number: " + q.getId());
+            System.out.println(q.getQuestion());
+            System.out.print(q.getOption1() + " ");
+            System.out.print(q.getOption2() + " ");
+            System.out.print(q.getOption3() + " ");
+            System.out.println(q.getOption4() + " ");
+            System.out.print("Your answer: ");
+            selection[i] = input.nextLine();
+            i++;
         }
+        System.out.println("Your answers: ");
+        for(String s : selection){
+            System.out.print(s + " ");
+        }
+        System.out.println();
+    }
+
+    public void printScore(){
+        int score = 0;
+
+        for(int i = 0; i< questions.length;i++){
+            Question que = questions[i];
+            String realAnswer = que.getAnswer();
+
+            String userAnswer = selection[i];
+
+            if(realAnswer.equalsIgnoreCase(userAnswer)){
+                score++;
+            }
+        }
+        System.out.println("Your score is: " + score);
     }
 
 }
