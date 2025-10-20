a530255b988a2ef90430a2d353ab39b2b5196e6e
Marko Palcok <marko.palcok@ibmix.hr>
2025-10-20 11:11:08 +0200

    add list and set

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
-public class Main {
-    public static void main(String[] args){
-        QuestionService service = new QuestionService();
-        service.displayQuestions();
-    }
+void main() {
+    //    final QuestionService service = new QuestionService();
+    //    service.displayQuestions();
+
+    List<String> listNames = Arrays.asList("Alice", "Bob", "Alice", "David", "Bob");
+    System.out.println("Original list: " + listNames);
+    Set<String> setNames = new HashSet<>(listNames);
+    System.out.println("Unique Names: " + setNames);
+
+
+//    Map<String, Integer> students = new HashMap<>();
+//    students.put("Mirko", 50);
+//    students.put("Slavko", 540);
+//    students.put("Duro", 5);
+//
+//    System.out.println(students.keySet());
+//
+//    for (String name : students.keySet()) {
+//        System.out.println(name + " " + students.get(name));
+//    }
+
 }
