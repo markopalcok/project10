1ef635c3e4b1c476255b48838de7c033cf87d588
Marko Palcok <marko.palcok@ibmix.hr>
2025-10-21 11:09:13 +0200

    feat/2010-2 - add stream

--- src/Main.java
+++ src/Main.java
@@ -45,5 +45,30 @@ void main() {
     final String name = scanner.nextLine();
     System.out.println("Hello " + name);
 
+    final List<String> listNames = Arrays.asList("Alice", "Bob", "Alice", "David", "Bob");
+    System.out.println("Original list: " + listNames);
+    final Set<String> setNames = new LinkedHashSet<>(listNames);
+    System.out.println("Unique Names: " + setNames);
+
+    final Map<String, Integer> students = new HashMap<>();
+    students.put("Mirko", 50);
+    students.put("Slavko", 540);
+    students.put("Duro", 5);
+
+    System.out.println(students.keySet());
+
+    for (final String name2 : students.keySet()) {
+        System.out.println(name2 + " " + students.get(name2));
+    }
+
+    final List<String> products = Arrays.asList("Laptop", "Pen", "Notebook", "Headphones", "Smartphone");
+    System.out.println("Original List: " + products);
+    final List<String> prodictsUpper = products.stream()
+                                               .map(String::toUpperCase)
+                                               .filter(count -> count.length() > 3)
+                                               .sorted()
+                                               .toList();
+    System.out.println("Filtered Products: " + prodictsUpper);
+
     scanner.close();
 }
